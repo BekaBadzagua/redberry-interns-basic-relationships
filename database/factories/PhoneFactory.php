521fe780e486c09bfb00d3d773b0590301b2ed8c
bekabadzagua <beka.badzagua@gmail.com>
2022-07-25 17:04:30 +0400

    db: implement relational seeder for user and phone

---
+++ database/factories/PhoneFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Phone>
+ */
+class PhoneFactory extends Factory
+{
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+			'provider'=> $this->faker->randomElement(['Magti', 'Beeline']),
+			'country' => $this->faker->country,
+			'number'  => $this->faker->phoneNumber(),
+		];
+	}
+}
