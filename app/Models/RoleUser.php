0819d01e223373598fe3b2eaa4318b94d7f10c85
bekabadzagua <beka.badzagua@gmail.com>
2022-07-25 20:49:00 +0400

    db: user and roles many to many relation seeder

---
+++ app/Models/RoleUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+* This Model is Made only to Seed DataBase.
+* if you only want to make relation you don't need to create this model.
+*/
+class RoleUser extends Model
+{
+	use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,59 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
+	private $ROLE_VALUES = ['admin', 'owner', 'member', 'guest'];
+
 	/**
 	 * Seed the application's database.
-	 *
-	 * @return void
 	 */
-	public function run()
+	public function run(): void
 	{
-		$this->call([
-			OneToOneSeeder::class,
-			OneToManySeeder::class,
-		]);
+		$USERS = 5;
+		$POSTS = 3;
+		$COMMENTS_PER_POST = 3;
+
+		# Execute in Any environment
+		foreach ($this->ROLE_VALUES as $role)
+		{
+			Role::create(['name'=>$role]);
+		}
+
+		# Execute only in local environment
+		if (App::environment('local'))
+		{
+			Post::factory()
+				->count($POSTS)
+				->hasComments($COMMENTS_PER_POST)
+				->create();
+
+			$users = User::factory()
+				->count($USERS)
+				->hasPhone()
+				->create();
+
+			$this->attachRolesToUsers($users);
+		}
+	}
+
+	/*
+	* Attaches random number of roles for users
+	*/
+	private function attachRolesToUsers(Collection $users): void
+	{
+		foreach ($users as $user)
+		{
+			$numberOfRoles = random_int(1, count($this->ROLE_VALUES));
+			$roles = Role::inRandomOrder()->limit($numberOfRoles)->get();
+			$user->roles()->attach($roles);
+		}
 	}
 }
