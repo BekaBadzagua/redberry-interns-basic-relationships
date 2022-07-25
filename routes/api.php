dcce93a3532f1deccce4faf57504d4a80d53f74d
bekabadzagua <beka.badzagua@gmail.com>
2022-07-25 18:01:15 +0400

    feat: user endpoints for Many to Many relationship

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
 	Route::get('users', 'index')->name('users.index');
+	// Endpoints for One to One Relationship
 	Route::get('users/{user}', 'get')->name('users.get');
 	Route::get('users/{user}/phone', 'phone')->name('users.phone');
 });
 
+// Endpoints for One to Many Relationship
 Route::controller(UserController::class)->group(function () {
 	Route::get('posts', 'index')->name('posts.index');
 	Route::get('posts/{post}', 'get')->name('posts.get');
 	Route::get('posts/{post}/comments', 'comments')->name('posts.comments');
+});
+
+Route::controller(UserController::class)->group(function () {
+	// Endpoints for Many to Many Relationship
+	Route::get('users/{user}/roles', 'roles')->name('users.roles');
 });
